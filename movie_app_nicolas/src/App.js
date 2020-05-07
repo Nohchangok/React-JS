a66954ee5548fccc26f4fad9dbb3c4a3d47cf08c
Nohel <ckddhr7560@naver.com>
2020-05-07 15:21:29 +0900

    #2.0 Creating your first React Component.

--- movie_app_nicolas/src/App.js
+++ movie_app_nicolas/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+function Potato(){
+    return <h1>I love Potato</h1>
+}
+
 function App() {
-    return <div><h1>Hello!!</h1></div>;
+    return (
+        <div>
+            <h1>Hello!!</h1>
+            <Potato />
+        </div>
+    );
+    
 }
 
 export default App;
--- movie_app_nicolas/src/index.js
+++ movie_app_nicolas/src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('potato'));
+//component하는 방법
+//jsx는 javscript안의 HTML.
+//react application은 한번에 하나에 componet만 rendering 할 수 있다.
+ReactDOM.render(<App /> , document.getElementById('potato'));
 

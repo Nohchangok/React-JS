8ec241cd20d522352f55545384a6d9f2915a274b
Nohel <ckddhr7560@naver.com>
2020-05-07 16:26:59 +0900

    #2.2 Dynamic Component Generation.

--- movie_app_nicolas/src/App.js
+++ movie_app_nicolas/src/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
 
-function Food({ fav }){ //props.fav == {fav}
-    return <h1>I like {fav}</h1>;
+function Food({ name, kor }){ //props.fav == {fav}
+    return <div>
+        <h2>I like {name}</h2>
+        <h2>나는 {kor}를 좋아해</h2>
+        </div>
 }
+
+const foodILike = [
+    {
+        name: "kimchi", kor: "김치" 
+    }, 
+    {
+        name: "samgyeopsal", kor: "삼겹살"
+    },
+    {
+        name: "bibimbap", kor: "비빔밥"
+    },
+    {
+        name: "doncasu", kor: "돈가스"
+    }]
 
 function App() {
     return (
         <div>
-            <h1>Hello!!</h1>
-            <Food fav="kimchi"/>
-            <Food fav="ramen"/>
-            <Food fav="samgiopsal"/>
-            <Food fav="chukumi"/>
+            {foodILike.map(dish => ( // == foodILike.map(function(dish){  })
+            <Food name ={dish.name} kor={dish.kor} /> //함수실행.
+            ))}
         </div>
     );
-    
 }
 
 export default App;
